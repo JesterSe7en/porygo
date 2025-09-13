37f721a0b718ace48cd8987c5d59ef7b3d1154a2
Alex Chan <alyxchan87@gmail.com>
2025-09-13 08:45:08 -0700

    Create thin wrapper around logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,14 +4,30 @@ import (
 	"go.uber.org/zap"
 )
 
-var Logger *zap.Logger
+var logger *zap.Logger
 
 func InitLogger() {
 	cfg := zap.NewDevelopmentConfig()
 
 	var err error
-	Logger, err = cfg.Build()
+	logger, err = cfg.Build()
 	if err != nil {
 		panic("failed to initialize logger " + err.Error())
 	}
+}
+
+func Info(msg string, fields ...any) {
+	logger.Sugar().Infow(msg, fields...)
+}
+
+func Warn(msg string, fields ...any) {
+	logger.Sugar().Warnw(msg, fields...)
+}
+
+func Error(msg string, fields ...any) {
+	logger.Sugar().Errorw(msg, fields...)
+}
+
+func Debug(msg string, fields ...any) {
+	logger.Sugar().Debugw(msg, fields...)
 }
