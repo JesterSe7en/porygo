0f6abbe11385e0bfbcc6dddf47c58addd8f5aa1c
Alex Chan <alyxchan87@gmail.com>
2025-09-23 10:59:28 -0700

    Clear cache from boltdb when calling the clear command
    Updated descriptions

--- cmd/cache/clear.go
+++ cmd/cache/clear.go
@@ -4,36 +4,28 @@
 package cache
 
 import (
-	"fmt"
-
+	"github.com/JesterSe7en/scrapego/internal/database"
 	"github.com/spf13/cobra"
 )
 
 // clearCmd represents the clear command
 var clearCmd = &cobra.Command{
 	Use:   "clear",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Clears the local cache of all scraped data.",
+	Long: `The clear command removes all stored responses from the local cache.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cache/clear called")
+By default, scrapego caches successful responses to avoid re-fetching the same URLs.
+Running this command will empty the cache, ensuring that the next scrape for any URL
+will fetch fresh data from the source. This is useful if the content of the URLs
+has changed and you need to force an update.
+
+Example:
+  scrapego cache clear`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return database.ClearCache()
 	},
 }
 
 func init() {
 	cacheCmd.AddCommand(clearCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// cache/clearCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// cache/clearCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
